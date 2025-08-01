3b94268d836845d5b5e7313d25d94fab48c8c0e0
Amanda Marques <amandamarquescardozorosa@gmail.com>
2025-07-31 22:14:16 -0300

    Desenvolvimento dos exemplos de Clean Code

--- src/main/java/com/senai/Aula08_CleanCode/Exemplos/Funcoes_Pequenas_e_Bem_Definidas.java
+++ src/main/java/com/senai/Aula08_CleanCode/Exemplos/Funcoes_Pequenas_e_Bem_Definidas.java
@@ -1,4 +1,58 @@
 package com.senai.Aula08_CleanCode.Exemplos;
 
 public class Funcoes_Pequenas_e_Bem_Definidas {
+   public void processar(int[] numeros){
+       int soma = 0;
+       int maior = Integer.MIN_VALUE;
+
+       for (int n : numeros){
+           soma += n;
+           if (n > maior){
+               maior = n;
+           }
+       }
+
+       System.out.println("Soma: " + soma);
+       System.out.println("Maior número: " + maior);
+   }
 }
+
+public void processar(int[] numeros){
+    int soma = calcularSoma(numeros);
+    int maior = encontrarMaior(numeros);
+
+    exibirResultados(soma, maior);
+}
+
+private int calcularSoma(int[] numeros){
+    int soma = 0;
+    for (int n : numeros){
+        soma += n;
+    }
+    return soma;
+}
+
+private int encontrarMaior(int[] numeros){
+    int maior = Integer.MIN_VALUE;
+    for (int n : numeros){
+        if (n > maior){
+            maior = n;
+        }
+    }
+    return maior;
+}
+
+private void exibirResultados(int soma, int maior){
+    System.out.println("Soma: " + soma);
+    System.out.println("Maior número: " + maior);
+}
+
+
+
+
+
+
+
+
+
+
